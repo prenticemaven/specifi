ccb0a3e8ef37e7ad6c17227c437e49c8373bd8fa
specifi <philip.specifi@gmail.com>
2014-07-15 01:53:36 +0800

    UI fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :projects
+  resources :materials
   root 'welcome#index'
+  authenticated :user do
+    root to: "dashboard#index", as: :authenticated_root
+  end
   get "about" => "welcome#about"
+  get "profile" => "profile#index"
+  get "settings" => "settings#index"
 end
